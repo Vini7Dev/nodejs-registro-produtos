2f1b6f8a93306437be56076f30e73156a8dcc16a
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-10-28 16:18:03 -0300

    Development of create section service

--- src/modules/users/controllers/SectionsController.ts
+++ src/modules/users/controllers/SectionsController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
+import CreateSectionService from '../services/CreateSectionService';
 
 class SectionsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    return response.status(201).json('OK');
+    const { email, password } = request.body;
+
+    const createSectionService = new CreateSectionService();
+
+    const userToken = await createSectionService.execute({
+      email,
+      password,
+    })
+
+    return response.status(201).json(userToken);
   }
 }
 
---
+++ src/modules/users/services/CreateSectionService.ts
@@ -0,0 +1,50 @@
+import { sign } from 'jsonwebtoken';
+import { compare } from 'bcrypt';
+
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+import AppError from '../../../shared/errors/AppError';
+import authConfig from '../../../config/authConfig';
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: User;
+    token: string;
+}
+
+class CreateSectionService {
+    private usersRepository: UsersRepository;
+
+    constructor() {
+        this.usersRepository = new UsersRepository();
+    }
+
+    public async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new AppError('Invalid credentials.', 401);
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch) {
+            throw new AppError('Invalid credentials.', 401);
+        }
+
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({}, secret, {
+            subject: user.id,
+            expiresIn,
+        });
+
+        return { user, token };
+    }
+}
+
+export default CreateSectionService;
